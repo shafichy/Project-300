70c789330c8287d390d82ff55a128cdab063fc0f
shafichy <shafichy1400@gmail.com>
2024-05-03 21:32:38 +0600

    added a banner

---
+++ src/components/Banner/Banner.jsx
@@ -0,0 +1,43 @@
+import metro1 from '../../assets/metro1.jpg'
+import metro2 from '../../assets/metro2.jpg'
+import metro3 from '../../assets/metro3.jpg'
+import metro4 from '../../assets/metro4.jpg'
+import { Navigation, Pagination, Scrollbar, A11y } from 'swiper/modules';
+import 'swiper/css/navigation';
+import 'swiper/css/pagination';
+import 'swiper/css/scrollbar';
+import { Swiper, SwiperSlide } from 'swiper/react';
+import 'swiper/css';
+const Banner = () => {
+    return (
+        <div>
+                <Swiper
+     modules={[Navigation, Pagination, Scrollbar, A11y]}
+     spaceBetween={50}
+     slidesPerView={1}
+     navigation
+     pagination={{ clickable: true }}
+     scrollbar={{ draggable: true }}
+      onSlideChange={() => console.log('slide change')}
+      onSwiper={(swiper) => console.log(swiper)}
+    >
+     
+      <SwiperSlide>
+        <img className='w-[1600px] h-[700px]' src={metro4} alt="" />
+      </SwiperSlide>
+      <SwiperSlide>
+         <img className='  w-[1600px] h-[700px]'  src={metro2} alt=""  />
+      </SwiperSlide>
+      <SwiperSlide>
+        <img className='w-[1600px] h-[700px]' src={metro3} alt="" />
+        </SwiperSlide>
+      <SwiperSlide>
+        <img className='w-[1600px] h-[700px]' src={metro1} alt="" />
+        </SwiperSlide>
+      
+    </Swiper>
+        </div>
+    );
+};
+
+export default Banner;
--- src/components/Home/Home.jsx
+++ src/components/Home/Home.jsx
@@ -1,9 +1,11 @@
+import Banner from "../Banner/Banner";
 
 
 const Home = () => {
     return (
         <div className="container mx-auto">
             <h1>This is home</h1>
+            <Banner></Banner>
         </div>
     );
 };
